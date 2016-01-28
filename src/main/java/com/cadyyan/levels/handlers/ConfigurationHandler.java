fbe69c6ec5ffc8315f393bc2790604ce101d1fa3
Cadyyan <cadyyan@avalon>
2016-01-28 06:41:53 -0600

    Remove the plugins setting
    We don't really need plugins right now

--- src/main/java/com/cadyyan/levels/handlers/ConfigurationHandler.java
+++ src/main/java/com/cadyyan/levels/handlers/ConfigurationHandler.java
@@ -48,8 +48,7 @@ public class ConfigurationHandler
 
 	public static void loadConfiguration()
 	{
-		Settings.Plugins.enabledMinecraft = configuration.getBoolean("minecraft", "skills", true, "Enable Minecraft plugin");
-		Settings.Skills.enabledCarpentry = configuration.getBoolean("skills", "carpentry", true, "Enable the carpentry skill");
+		Settings.Skills.enabledCarpentry = configuration.getBoolean("carpentry", "skills", true, "Enable the carpentry skill");
 	}
 
 	@SubscribeEvent
